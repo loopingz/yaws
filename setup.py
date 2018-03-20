25efc3d6cf16afb3675484b2da53293b549d3a9b
Remi Cattiau <remi@cattiau.com>
2018-03-20 16:33:22 -0700

    Update to 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yaws',
-    version='1.0.0',
+    version='1.1.0',
     description='Utils for AWS cloud',
     url='https://github.com/loopingz/yaws',
     author='Loopingz',
